2a6588279b42b2bc2a9634a079890508b3633944
Jesse Griffin <jesse.griffin@gmail.com>
2020-01-31 01:58:19 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,11 +1,3 @@
-def reverse_each_word(string)
-  string = string.split('')
-  reverse_each_word2 []
-  
-  string.each do |reverse|
-    reverse_each_word2.unshift(reverse)
-  end
-  
-  return reverse_each_word2.join('')
+def reverse_each_word(sentence)
   
 end 
